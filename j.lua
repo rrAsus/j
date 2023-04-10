1e4d1d71df109bd36cc8bbce49c7c2f2c9b8bca6
Christian <102998736+rrAsus@users.noreply.github.com>
2023-04-10 15:59:42 +0200

    Create j.lua

---
+++ j.lua
@@ -0,0 +1,13 @@
+keys = {
+[0YQtweF57DKa] = true,
+[Aif0KxZEJJsy] = true, 
+[aJKmUS6NMjPl] = true, 
+[zWZNKn8AkHYG] = true,
+[Ghz9EytIQtDI] = true,
+[UsDIKou3t0l3] = true,
+[ojID0PIvVrnv] = true,
+[q9fs5hKXyYDh] = true,
+[p43BemzUywLX] = true,
+[w5wHbSwxAc1a] = true
+}
+return keys
